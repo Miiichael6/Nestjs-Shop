cbab1196fedf75883d2aa97b491b83d59cb4130a
Michael Canales <CDLSMichael.16@gmail.com>
2023-06-24 19:20:11 -0500

    fixed docs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -14,7 +14,7 @@ export class CreateProductDto {
   @ApiProperty({
     description: "Product title (unique)",
     nullable: false,
-    minLength: 1
+    minLength: 1,
   })
   @IsString()
   @MinLength(1)
@@ -25,42 +25,41 @@ export class CreateProductDto {
   @IsPositive()
   @IsOptional()
   price?: number;
-  
+
   @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
-  
+
   @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
-  
+
   @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
-  stock?: number; 
-  
+  stock?: number;
+
   @ApiProperty()
   @IsString({ each: true })
   @IsArray()
-  sizes: string[]
-  
+  sizes: string[];
+
   @ApiProperty()
-  @IsIn(['men','women','kid','unisex'])
+  @IsIn(["men", "women", "kid", "unisex"])
   gender: string;
-  
+
   @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
-  
-  
+
   @ApiProperty()
-  @IsString({each: true})
+  @IsString({ each: true })
   @IsArray()
   @IsOptional()
-  images?:string[]
+  images?: string[];
 }
